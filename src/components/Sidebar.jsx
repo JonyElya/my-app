441b778af41d540f1d1908370b424e87f1b28a76
JonyElya <38189777+JonyElya@users.noreply.github.com>
2020-01-19 05:07:40 +0700

    added new file

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <nav className="sidebar">
+      <div>
+        <a>Profile</a>
+      </div>
+      <div>
+        <a>Photo</a>
+      </div>
+      <div>
+        <a>music</a>
+      </div>
+    </nav>
+  );
+};
+export default Sidebar;
